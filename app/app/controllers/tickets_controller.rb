6b746de1dd176aadccfb112c5e77a518ac84c8cf
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-26 23:52:49 +0100

    dodaje pole na telefon

--- app/app/controllers/tickets_controller.rb
+++ app/app/controllers/tickets_controller.rb
@@ -69,6 +69,6 @@ class TicketsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ticket_params
-      params.require(:ticket).permit(:name, :address, :price, :email_address, :seat_id_seq)
+      params.require(:ticket).permit(:name, :address, :price, :email_address, :phone, :seat_id_seq)
     end
 end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126222357) do
+ActiveRecord::Schema.define(version: 20170126224954) do
 
   create_table "tickets", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170126222357) do
     t.string   "seat_id_seq"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "phone"
   end
 
 end
