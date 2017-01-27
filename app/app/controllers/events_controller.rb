8cb2cbf259ef90f51188603db0a4ccc95803e3cd
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-27 01:05:25 +0100

    dodaje liste wydarzeń

--- app/app/controllers/events_controller.rb
+++ app/app/controllers/events_controller.rb
@@ -1,10 +1,19 @@
 class EventsController < ApplicationController
   def index
+      @events = Event.all
   end
 
   def new
+      @event = Event.new
   end
 
   def create
+      @event = Event.new(params[:event])
+      if @event.save
+          flash[:message] = 'Event został poprawnie stworzony.'
+          redirect_to events_path
+      else
+          render :action => "new"
+      end
   end
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'events/index'
-
-  get 'events/new'
-
-  get 'events/create'
+  get 'index/index'
 
   resources :tickets
-  root :to => "tickets#index"
+  root :to => "index#index"
+  resources :events, :only => [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
