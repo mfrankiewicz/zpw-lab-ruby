bdbc966ccc0ca582cf144a268ce44bea24610ecb
Michał Frankiewicz <michal@frankiewicz.me>
2017-01-28 21:04:47 +0100

    dodaje edytowanie i usuwanie eventów

--- app/app/controllers/events_controller.rb
+++ app/app/controllers/events_controller.rb
@@ -1,22 +1,53 @@
 class EventsController < ApplicationController
-  def index
-      @events = Event.all
-  end
+    before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+    def index
+        @events = Event.all
+    end
 
-  def new
-      @event = Event.new
-  end
+    def new
+        @event = Event.new
+    end
 
-  def create
-      @event = Event.new(event_params)
-      if @event.save
-          flash[:message] = 'Event został poprawnie stworzony.'
-          redirect_to events_path
-      else
-          render :action => "new"
-      end
-  end
+    def edit
+    end
 
+    def show
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            flash[:message] = 'Event has been successfully added.'
+            redirect_to events_path
+        else
+            render :action => "new"
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @event.update(event_params)
+                format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+                format.json { render :show, status: :ok, location: @event }
+            else
+                format.html { render :edit }
+                format.json { render json: @event.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @event.destroy
+        respond_to do |format|
+            format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+            format.json { head :no_content }
+        end
+    end
+
+    private
+    def set_ticket
+        @event = Event.find(params[:id])
+    end
     def event_params
         params.require(:event).permit(:artist, :description, :price_low, :price_high, :seat_count, :event_date, :adult)
     end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :tickets
   root :to => "index#index"
-  resources :events, :only => [:index, :new, :create]
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
